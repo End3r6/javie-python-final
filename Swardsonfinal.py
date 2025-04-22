500fa3f77019c9766aa2c48d87b5bfa0a9587527
End3r6 <jd@swardson.com>
2025-04-21 22:40:05 -0600

    Update Swardsonfinal.py

--- Swardsonfinal.py
+++ Swardsonfinal.py
@@ -1,3 +1,5 @@
+import tkinter
+from tkinter import *
 import random 
 
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,9 +20,53 @@ if nums:
 if syms:
     all += symbols 
 
-length = 20 
-amount = 10 
 
-for x in range(amount):
-        password = "".join((random.sample)(all, length)) 
-        print(password) 
+password = ""
+
+# entry for length
+# entry for amount
+# button to generate, uses the generate function
+# Text to show generated passwords
+
+# once the generate button is pressed, it gets the numbers from length and amount inputs
+# this will use the .get() function in the entries
+# it will generate the passwords and use the .insert(index (ie INSERT) string)
+
+
+top = tkinter.Tk()
+
+
+L1 = Label(top, text="Length")
+L1.pack( side = TOP)
+E1 = Entry(top, bd =5)
+E1.pack(side = TOP)
+
+L2 = Label(top, text="amount")
+L2.pack( side = TOP)
+E2 = Entry(top, bd =5)
+E2.pack(side = TOP)
+
+text = Text(top)
+
+def generate():
+    if E1.get() != "":
+        length = int(E1.get())
+    else:
+        length = 20
+
+    if E2.get() != "":
+        amount = int(E2.get())
+    else:
+        amount = 10
+
+    for x in range(amount):
+        password = "".join((random.sample)(all, length))
+        print(password)
+        text.insert(INSERT, password+'\n')
+        text.pack()
+
+B = Button(top, text ="generate", command = generate)
+B.pack(side="bottom", fill="x")
+
+# Code to add widgets will go here...
+top.mainloop()
